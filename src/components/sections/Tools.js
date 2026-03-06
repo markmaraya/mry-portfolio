3d1e518d5b068d8db6ffd6fed97c5c3377955764
Mark Maraya <markanthonymaraya@gmail.com>
2026-03-06 23:55:45 +0800

    chore: updated skills list

--- src/components/sections/Tools.js
+++ src/components/sections/Tools.js
@@ -6,17 +6,13 @@ const skills = [
   { name: "TypeScript", icon: "logos:typescript-icon" },
   { name: "React", icon: "logos:react" },
   { name: "Tailwind CSS", icon: "logos:tailwindcss-icon" },
-  { name: "Material UI", icon: "logos:material-ui" },
-  { name: "KnockoutJS", icon: "file-icons:knockout", color: "text-red-600" },
-  { name: "jQuery", icon: "devicon:jquery" },
-  { name: "HTML5", icon: "devicon:html5" },
-  { name: "CSS3", icon: "devicon:css3" },
+  { name: "Material UI", icon: "logos:material-ui" },  
   { name: "Sass/SCSS", icon: "logos:sass" },
   { name: "Figma", icon: "logos:figma" },
+  { name: "HTML5", icon: "devicon:html5" },
+  { name: "CSS3", icon: "devicon:css3" },
   { name: "Git/GitHub", icon: "logos:git-icon" },
   { name: "npm", icon: "devicon:npm-wordmark" },
-  { name: ".Net", icon: "logos:dotnet" },
-  { name: "SQL", icon: "logos:mysql" },
 ];
 
 const Tools = ({ ...props }) => {
