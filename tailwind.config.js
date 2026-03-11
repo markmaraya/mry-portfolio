eb9b7678dfdfa1e0d9b84f11b627bce03883da99
Mark Maraya <markanthonymaraya@gmail.com>
2026-03-11 21:37:03 +0800

    chore: add extended rgb color palette and update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         muted: "rgb(var(--color-muted) / <alpha-value>)",
         link: "rgb(var(--color-link) / <alpha-value>)",
         linkHover: "rgb(var(--color-link-hover) / <alpha-value>)",
+        light: "rgb(var(--color-light) / <alpha-value>)",
+        dark: "rgb(var(--color-dark) / <alpha-value>)",
+        highlight: "rgb(var(--color-highlight) / <alpha-value>)",
+        info: "rgb(var(--color-info) / <alpha-value>)",
+        success: "rgb(var(--color-success) / <alpha-value>)",
+        warning: "rgb(var(--color-warning) / <alpha-value>)",
+        error: "rgb(var(--color-error) / <alpha-value>)",
       },
       fontFamily: {
         hero: ["Darumadrop One", "cursive"],
