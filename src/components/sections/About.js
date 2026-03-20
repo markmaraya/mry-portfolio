cc44df17175171369d8aceed3cdce3e42a86d860
Mark Maraya <markanthonymaraya@gmail.com>
2026-03-21 00:12:20 +0800

    feat: add SVG illustration to About section

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,9 +1,13 @@
+import ProgressiveWebAppSVG from "../svg/ProgressiveWebAppSVG";
+
 const About = ({ ...props }) => {
   return (
     <section {...props}>
       <h3 className="text-accent text-5xl font-hero mb-6">About</h3>
       <div className="relative max-w-7xl mx-auto grid md:grid-cols-2 gap-12 text-left text-text p-10">
-        <div className="flex items-center justify-center"></div>
+        <div className="flex items-center justify-center">
+          <ProgressiveWebAppSVG />
+        </div>
         <div>
           <p className="leading-relaxed mb-6">
             I’m a Front-End Developer based in Manila, Philippines.
