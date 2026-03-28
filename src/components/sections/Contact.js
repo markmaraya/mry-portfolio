21a821ff6697261f4629a0f5eeeeedbc8225bf04
Mark Maraya <markanthonymaraya@gmail.com>
2026-03-28 19:29:21 +0800

    feat: add arrow icon to ContactItem and update hover effect

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -37,9 +37,7 @@ const Contact = () => {
       id="contact"
       className="h-[calc(100vh-6rem)] bg-bg flex flex-col items-center justify-center"
     >
-      <h3 className="text-accent text-5xl font-hero mb-16">
-        Contact
-      </h3>
+      <h3 className="text-accent text-5xl font-hero mb-16">Contact</h3>
       <div className="max-w-6xl mx-auto grid grid-cols-2 gap-8">
         {contacts.map((skill, index) => (
           <ContactItem
@@ -62,7 +60,9 @@ const ContactItem = ({ iconPlain, iconOriginal, label, link, hoverColor }) => {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="group bg-main/60 grid w-full sm:w-96 border border-accent rounded-xl h-20 overflow-hidden"
+      className="group relative grid w-full sm:w-96 h-20 
+      border border-accent rounded-xl overflow-hidden 
+      bg-main/60 hover:border-secondary"
     >
       <div className="relative">
         <Icon
@@ -84,11 +84,19 @@ const ContactItem = ({ iconPlain, iconOriginal, label, link, hoverColor }) => {
         />
       </div>
       <span
-        className="text-lg font-medium text-accent translate-x-8
-        transition-transform duration-300 ease-out group-hover:translate-x-20"
+        className="text-lg font-medium text-text translate-x-8
+        transition-transform duration-300 ease-out
+        group-hover:translate-x-20 group-hover:text-linkHover"
       >
         {label}
       </span>
+      <Icon
+        icon="mdi:keyboard-arrow-right"
+        className="absolute right-6 top-1/2 h-6 w-6 inline-block
+        -translate-y-1/2 rotate-[-45deg] text-text
+        transition-transform duration-300 ease-out
+        group-hover:scale-150 group-hover:translate-x-4 group-hover:-translate-y-7 group-hover:text-linkHover"
+      />
     </a>
   );
 };
